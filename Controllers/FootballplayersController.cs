a7256ca5b34c4341be80367eb85b4af69d157161
Zyhler <icyxgram@gmail.com>
2022-12-08 18:45:11 +0100

    Add project files.

---
+++ Controllers/FootballplayersController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment1;
+using FootballPlayerREST.Managers;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkPlayerID=397860
+
+namespace FootballPlayerREST.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class FootballplayersController : ControllerBase
+    {
+        // GET: api/<FootballPlayerController>
+        private readonly FootballPlayersManager _manager = new FootballPlayersManager();
+
+        // GET: api/<FootballPlayersController>
+        [HttpGet]
+        public IEnumerable<FootballPlayer> GetAll()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<FootballPlayersController>/5
+        [HttpGet("{PlayerID}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public ActionResult<FootballPlayer> GetByPlayerID(int PlayerID)
+        {
+            FootballPlayer footballPlayer = _manager.GetByPlayerID(PlayerID);
+            if(footballPlayer == null) return NotFound("No Such FootballPlayer with PlayerID:"+PlayerID);
+            return Ok(footballPlayer);
+        }
+
+        // POST api/<FootballPlaersController>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public ActionResult<FootballPlayer> Add([FromBody] FootballPlayer value)
+        {
+            //FootballPlayer FootballPlayer = new FootballPlayer(value);
+            FootballPlayer footballPlayer = value;
+            footballPlayer = _manager.Add(value);
+            return Created("/"+footballPlayer.PlayerID,footballPlayer);
+        }
+
+        // PUT api/<FootballPlayersController>/5  - Opgaven bad ikke om PUT / Update
+        //[HttpPut("{PlayerID}")]
+        //public FootballPlayer Put(int PlayerID, [FromBody] FootballPlayer value)
+        //{
+        //    return _manager.Update(PlayerID, value);
+        //} 
+
+        // DELETE api/<FootballPlayersController>/5
+        [HttpDelete("{PlayerID}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public ActionResult<FootballPlayer> Delete(int PlayerID)
+        {
+            FootballPlayer footballPlayer = _manager.GetByPlayerID(PlayerID);
+            if(footballPlayer == null) return NotFound("No Such FootballPlayer with PlayerID:"+PlayerID);
+            return Ok(_manager.Delete(PlayerID));
+            
+        }
+    }
+}
---
+++ Managers/FootballPlayersManager.cs
@@ -0,0 +1,58 @@
+﻿using Assignment1;
+using FootballPlayerREST;
+using Microsoft.AspNetCore.Http;
+
+namespace FootballPlayerREST.Managers
+{
+    public class FootballPlayersManager
+    {
+            private static int _nextPlayerID = 10;
+            private static readonly List<FootballPlayer> ListOfAthletes = new List<FootballPlayer>
+            {
+            new FootballPlayer { PlayerID = _nextPlayerID++, Name = "jon dahl", Age = 25, ShirtNumber = 4 },
+            new FootballPlayer { PlayerID = _nextPlayerID++, Name = "Markus ericson", Age = 28, ShirtNumber = 15 },
+            new FootballPlayer { PlayerID = _nextPlayerID++, Name = "messi", Age = 30, ShirtNumber = 28 },
+            new FootballPlayer { PlayerID = _nextPlayerID++, Name = "ronaldo ", Age = 55, ShirtNumber = 18 },
+            // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guPlayerIDe/classes-and-structs/object-and-collection-initializers
+            };
+
+            public List<FootballPlayer> GetAll()
+            {
+                return new List<FootballPlayer>(ListOfAthletes);
+                // copy constructor
+                // Callers should no get a reference to the ListOfObjects object, but rather get a copy
+            }
+
+            public FootballPlayer GetByPlayerID(int PlayerID)
+            {
+
+                return ListOfAthletes.Find(footballPlayer => footballPlayer.PlayerID == PlayerID);
+            }
+
+            public FootballPlayer Add(FootballPlayer newFootballPlayer)
+            {
+                newFootballPlayer.PlayerID = _nextPlayerID++;
+                ListOfAthletes.Add(newFootballPlayer);
+                return newFootballPlayer;
+            }
+
+            public FootballPlayer Delete(int PlayerID)
+            {
+                FootballPlayer footballPlayer = ListOfAthletes.Find(footballPlayer => footballPlayer.PlayerID == PlayerID);
+                if (footballPlayer == null) return null;
+                ListOfAthletes.Remove(footballPlayer);
+                return footballPlayer;
+            }
+
+            public FootballPlayer Update(int PlayerID, FootballPlayer updates)
+            {
+                FootballPlayer footballPlayer = ListOfAthletes.Find(footballPlayer1 => footballPlayer1.PlayerID == PlayerID);
+                if (footballPlayer == null) return null;
+                footballPlayer.Name = updates.Name;
+                footballPlayer.Age = updates.Age;
+                footballPlayer.ShirtNumber = updates.ShirtNumber;
+                return footballPlayer;
+            }
+        
+    }
+}
